ef3f5ef91b25cc0f2477c6830949cd78d076fbb8
Igorgusakov199918@gmail.com <Igorgusakov199918@gmail.com>
2023-02-10 19:20:31 +0600

    version 2.0

--- buttonScroll.js
+++ buttonScroll.js
@@ -1,32 +1,15 @@
-var scrollUpButton = document.getElementById("scroll-up-button");
 
-// When the user scrolls down 1000px from the top of the document, show the button
-window.onscroll = function() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    scrollUpButton.style.zIndex = "0";
-    scrollUpButton.style.display = "block";
-    scrollUpButton.classList.add("show");
-    scrollUpButton.style.animation = "openScroll 0.6s 1";
-    scrollUpButton.style.animationFillMode = "both";
-  } else if (document.body.scrollTop < 600 || document.documentElement.scrollTop < 600) {
-    scrollUpButton.style.zIndex = "-100";
-    scrollUpButton.style.animation = "closeScroll 0.6s 1";
-    scrollUpButton.style.animationFillMode = "both";
-  } else if (document.body.scrollTop < 400 || document.documentElement.scrollTop < 400) {
-    scrollUpButton.style.display = "none";
-    scrollUpButton.classList.remove("show");
-  }
-};
+let theme = document.querySelector('.theme')
+let dark1 = document.querySelectorAll('.darkA')
+let dark2 = document.querySelectorAll('.darkB')
 
-// When the user clicks on the button, scroll to the top of the document
-scrollUpButton.onclick = function() {
-    scrollToTop();
-  };
-  
-  function scrollToTop() {
-    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-    if (currentScroll > 0) {
-      window.requestAnimationFrame(scrollToTop);
-      window.scrollTo(0, currentScroll - currentScroll / 8);
-    }
-  }
+theme.onclick = function() {
+for (let i=0; i<dark1.length; i++){
+  dark1[i].classList.toggle("dark1")
+}
+for (let i=0; i<dark1.length; i++){
+  dark2[i].classList.toggle("dark2")
+}
+}
+
+console.log(dark1[0])
